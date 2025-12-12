429a096ecaa2b1dcbb8370e4fe1c2a2ae8fa1402
Md salim islam <salimislam0036@gmail.com>
2025-12-12 22:12:08 +0600

    data type premitive and reference

--- app.js
+++ app.js
@@ -1,15 +1,13 @@
+"use strict";
 // var a = 12;
 // function abcd() {
 //     console.log(a );
 // }
-
-
+Object.defineProperty(exports, "__esModule", { value: true });
 // function abcd() {
 //     let x = 12;
 //     let y = 22;
 // }
-
-
 // premitive and referance 
 // 12
 // salim
@@ -21,6 +19,6 @@
 // false
 // 12.2
 // ()
-
 // primitive direct copy kora jai ,,,
 // reference copy kora jai na
+//# sourceMappingURL=app.js.map
